48b93c1f1a561517950dcc4d428dc5f085cc5906
Nadia Gonzalez <yologonzalezv@gmail.com>
2023-04-01 18:17:57 -0600

    eliminamos y creamos scripts nuevos
    escribimos scripts mas optimizados y ordenamos , mas los cuartos base de nuestra mazmorra

---
+++ Mazmorras 3D Generador/Assets/scripts/RoomTemplates.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTemplates : MonoBehaviour
+{
+    //Aqui van a estar todos los prefabs de las salas para saber donde se encuentran las salidas
+
+    public GameObject[] bootomRooms; //array por que podemos tener mas de  una habitacion con salidas abajo
+    public GameObject[] TopRooms;
+    public GameObject[] LeftRooms;
+    public GameObject[] RightRooms;
+
+
+}
